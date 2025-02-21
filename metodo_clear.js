c869eebcf6c90836c6c5ab13d5790bc8c0dca13b
Aderaldo Anderson Silva Vasconcelos Amorim <fatalhouse2@hotmail.com>
2025-02-21 10:49:57 -0300

    Update and rename localStorage.js to metodo_clear.js

---
+++ metodo_clear.js
@@ -0,0 +1,19 @@
+/* 
+
+@author Aderaldo Anderson S. V. Amorim
+
+MÉTODO
+
+clear()
+
+para facilitar o teste, irei criar uma função que executa a ação clear()
+
+*/
+
+function limparLocalStorage() {
+  // Nesse caso não é necessário passar nenhum argumento
+  localStorage.clear();
+}
+
+// Chamando a Função
+limparLocalStorage();
